f69299674ca43929c4a4c7c673228904bfa3a70c
aldelaro5 <aldelaro5@gmail,com>
2021-05-08 11:15:04 -0400

    Initial commit

---
+++ Patches/InputIO.cs
@@ -0,0 +1,56 @@
+﻿using HarmonyLib;
+using InputIOManager;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+
+namespace BugFables.SaveRedirection
+{
+  public class InputIOPatches
+  {
+    public static string folderRedirect { get; set; }
+
+    [HarmonyPrefix]
+    [HarmonyPatch(typeof(InputIO), nameof(InputIO.CreateFile))]
+    static bool RedirectCreateFile(ref string path, string content)
+    {
+      path = folderRedirect + path;
+      return true;
+    }
+
+    [HarmonyPrefix]
+    [HarmonyPatch(typeof(InputIO), nameof(InputIO.ReadFile))]
+    [HarmonyPatch(typeof(InputIO), nameof(InputIO.DeleteFile))]
+    static bool RedirectReadDelete(ref string path)
+    {
+      path = folderRedirect + path;
+      return true;
+    }
+    
+    [HarmonyTranspiler]
+    [HarmonyPatch(typeof(InputIO), nameof(InputIO.LoadSettings))]
+    static IEnumerable<CodeInstruction> RedirectConfig(IEnumerable<CodeInstruction> instructions)
+    {
+      var codeMatch = new CodeMatcher(instructions);
+      codeMatch = codeMatch.MatchForward(false, new CodeMatch(OpCodes.Ldstr, "config.dat"));
+      codeMatch = codeMatch.Repeat(matcher =>
+      {
+        matcher.SetOperandAndAdvance(folderRedirect + matcher.Operand);
+      });
+      return codeMatch.InstructionEnumeration();
+    }
+    
+    [HarmonyTranspiler]
+    [HarmonyPatch(typeof(InputIO), nameof(InputIO.Save))]
+    [HarmonyPatch(typeof(InputIO), nameof(InputIO.SaveExists))]
+    static IEnumerable<CodeInstruction> RedirectSaves(IEnumerable<CodeInstruction> instructions)
+    {
+      var codeMatch = new CodeMatcher(instructions);
+      codeMatch = codeMatch.MatchForward(false, new CodeMatch(OpCodes.Ldstr, "save"));
+      codeMatch = codeMatch.Repeat(matcher =>
+      {
+        matcher.SetOperandAndAdvance(folderRedirect + matcher.Operand);
+      });
+      return codeMatch.InstructionEnumeration();
+    }
+  }
+}
---
+++ SaveRedirectionPlugin.cs
@@ -0,0 +1,18 @@
+﻿using BepInEx;
+using HarmonyLib;
+using System.IO;
+
+namespace BugFables.SaveRedirection
+{
+  [BepInPlugin("com.aldelaro5.BugFables.plugins.SaveRedirection", "Save Redirection", "1.0.0")]
+  [BepInProcess("Bug Fables.exe")]
+  public class SaveRedirectionPlugin : BaseUnityPlugin
+  {
+    public void Awake()
+    {
+      InputIOPatches.folderRedirect = Paths.BepInExRootPath + "\\";
+      var harmony = new Harmony("com.aldelaro5.BugFables.plugins.SaveRedirection");
+      harmony.PatchAll(typeof(InputIOPatches));
+    }
+  }
+}
